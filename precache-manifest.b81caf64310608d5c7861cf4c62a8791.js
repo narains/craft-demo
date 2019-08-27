0eec73befa50440d30a00108dbb9d0113568993a
Shruti Narain <shnarain@cisco.com>
2019-08-26 22:23:35 -0400

    Updates

---
+++ precache-manifest.b81caf64310608d5c7861cf4c62a8791.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5c94929b99be37298c0c7ac8b95071c",
+    "url": "/craft-demo/index.html"
+  },
+  {
+    "revision": "d62df7b1728edb50165d",
+    "url": "/craft-demo/static/css/main.b4361d76.chunk.css"
+  },
+  {
+    "revision": "ef0ba492d1a28df94f8b",
+    "url": "/craft-demo/static/js/2.671bae85.chunk.js"
+  },
+  {
+    "revision": "d62df7b1728edb50165d",
+    "url": "/craft-demo/static/js/main.2e440d7d.chunk.js"
+  },
+  {
+    "revision": "c2b8735fff8fb1ea0543",
+    "url": "/craft-demo/static/js/runtime~main.40db0a52.js"
+  }
+]);
